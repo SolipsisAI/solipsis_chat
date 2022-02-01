c3a864ca6f459fa31ea4e9b74e871fd4d282ae7b
Bitjockey <fastjack@jackpoint.network>
2022-01-31 21:41:19 -0500

    Use bubble component to customize bubble appearance

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:flutter_chat_types/flutter_chat_types.dart' as types;
 import 'package:flutter_chat_ui/flutter_chat_ui.dart';
+import 'package:bubble/bubble.dart';
 
 // For the testing purposes, you should probably use https://pub.dev/packages/uuid
 String randomString() {
@@ -54,6 +55,28 @@ class _SolipsisChatHomeState extends State<SolipsisChatHome> {
     _addMessage(textMessage);
   }
 
+  Widget _bubbleBuilder(
+    Widget child, {
+    required message,
+    required nextMessageInGroup,
+  }) {
+    return Bubble(
+      child: child,
+      color: _user.id != message.author.id ||
+              message.type == types.MessageType.image
+          ? const Color(0xfff5f5f7)
+          : const Color(0xff6f61e8),
+      margin: nextMessageInGroup
+          ? const BubbleEdges.symmetric(horizontal: 6)
+          : null,
+      nip: nextMessageInGroup
+          ? BubbleNip.no
+          : _user.id != message.author.id
+              ? BubbleNip.leftBottom
+              : BubbleNip.rightBottom,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -63,6 +86,7 @@ class _SolipsisChatHomeState extends State<SolipsisChatHome> {
           messages: _messages,
           onSendPressed: _handleSendPressed,
           user: _user,
+          bubbleBuilder: _bubbleBuilder,
         ),
       ),
     );
