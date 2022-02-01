0229ceb685630b72190494df3e7dc3e190a5ff14
Bitjockey <fastjack@jackpoint.network>
2022-01-31 21:55:26 -0500

    Enable pagination

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_chat_types/flutter_chat_types.dart' as types;
 import 'package:flutter_chat_ui/flutter_chat_ui.dart';
 import 'package:bubble/bubble.dart';
+import 'package:http/http.dart' as http;
 
 // For the testing purposes, you should probably use https://pub.dev/packages/uuid
 String randomString() {
@@ -35,8 +36,36 @@ class SolipsisChatHome extends StatefulWidget {
 }
 
 class _SolipsisChatHomeState extends State<SolipsisChatHome> {
-  final List<types.Message> _messages = [];
+  int _page = 0;
+  List<types.Message> _messages = [];
   final _user = const types.User(id: '06c33e8b-e835-4736-80f4-63f44b66666c');
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  Future<void> _handleEndReached() async {
+    final uri = Uri.parse(
+      'https://api.instantwebtools.net/v1/passenger?page=$_page&size=20',
+    );
+    final response = await http.get(uri);
+    final json = jsonDecode(response.body) as Map<String, dynamic>;
+    final data = json['data'] as List<dynamic>;
+    final messages = data
+        .map(
+          (e) => types.TextMessage(
+            author: _user,
+            id: e['_id'] as String,
+            text: e['name'] as String,
+          ),
+        )
+        .toList();
+    setState(() {
+      _messages = [..._messages, ...messages];
+      _page = _page + 1;
+    });
+  }
 
   void _addMessage(types.Message message) {
     setState(() {
@@ -87,6 +116,7 @@ class _SolipsisChatHomeState extends State<SolipsisChatHome> {
           onSendPressed: _handleSendPressed,
           user: _user,
           bubbleBuilder: _bubbleBuilder,
+          onEndReached: _handleEndReached,
         ),
       ),
     );
