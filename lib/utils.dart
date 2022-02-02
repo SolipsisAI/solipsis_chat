bb890c2f43a90260203a6759b564aa198cd7f149
Bitjockey <fastjack@jackpoint.network>
2022-02-02 00:21:12 -0500

    Set constant for api url

--- lib/utils.dart
+++ lib/utils.dart
@@ -3,6 +3,9 @@ import 'dart:convert';
 import 'package:flutter_chat_types/flutter_chat_types.dart' as types;
 import 'package:http/http.dart' as http;
 
+const String loremIpsumApiUrl =
+    'https://litipsum.com/api/pride-and-prejudice/1/json';
+
 String randomString() {
   final random = Random.secure();
   final values = List<int>.generate(16, (i) => random.nextInt(255));
@@ -10,9 +13,7 @@ String randomString() {
 }
 
 Future<types.Message> randomMessage(types.User user) async {
-  final uri = Uri.parse(
-    'https://litipsum.com/api/pride-and-prejudice/1/json',
-  );
+  final uri = Uri.parse(loremIpsumApiUrl);
   final response = await http.get(uri);
   final json = jsonDecode(response.body) as Map<String, dynamic>;
   final data = json['text'] as List<dynamic>;
