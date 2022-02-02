acd6919b17b616c47f7a1bb0149c690e562d5ad2
Bitjockey <fastjack@jackpoint.network>
2022-02-02 00:17:10 -0500

    Move chat widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,5 @@
-import 'dart:developer' as logger;
-import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:flutter_chat_types/flutter_chat_types.dart' as types;
-import 'package:flutter_chat_ui/flutter_chat_ui.dart';
-import 'package:bubble/bubble.dart';
-import 'package:http/http.dart' as http;
-import 'utils.dart';
+import 'chat.dart';
 
 void main() {
   runApp(const SolipsisChat());
@@ -18,113 +12,6 @@ class SolipsisChat extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       home: SolipsisChatHome(),
-    );
-  }
-}
-
-class SolipsisChatHome extends StatefulWidget {
-  const SolipsisChatHome({Key? key}) : super(key: key);
-
-  @override
-  _SolipsisChatHomeState createState() => _SolipsisChatHomeState();
-}
-
-class _SolipsisChatHomeState extends State<SolipsisChatHome> {
-  int _page = 0;
-  List<types.Message> _messages = [];
-  final _user = const types.User(id: '06c33e8b-e835-4736-80f4-63f44b66666c');
-  final _bot = const types.User(id: '09778d0f-fb94-4ac6-8d72-96112805f3ad');
-
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  Future<void> _handleEndReached() async {
-    final uri = Uri.parse(
-      'https://reqres.in/api/unknown?page=$_page&per_page=20',
-    );
-    final response = await http.get(uri);
-    final json = jsonDecode(response.body) as Map<String, dynamic>;
-    final data = json['data'] as List<dynamic>;
-    logger.log('data: $data');
-    final messages = data
-        .map(
-          (e) => types.TextMessage(
-            author: _user,
-            id: '$e["id"]',
-            text: e['name'] as String,
-          ),
-        )
-        .toList();
-    logger.log('messages: $messages');
-    setState(() {
-      _messages = [..._messages, ...messages];
-      _page = _page + 1;
-    });
-  }
-
-  Future<void> _handleBotResponse() async {
-    final message = await randomMessage(_bot);
-    _addMessage(message);
-  }
-
-  void _addMessage(types.Message message) {
-    setState(() {
-      _messages.insert(0, message);
-    });
-    logger.log('data: $message');
-  }
-
-  void _handleSendPressed(types.PartialText message) {
-    final textMessage = types.TextMessage(
-      author: _user,
-      createdAt: DateTime.now().millisecondsSinceEpoch,
-      id: randomString(),
-      text: message.text,
-    );
-
-    _addMessage(textMessage);
-    _handleBotResponse();
-  }
-
-  Widget _bubbleBuilder(
-    Widget child, {
-    required message,
-    required nextMessageInGroup,
-  }) {
-    return Bubble(
-      child: child,
-      color: _user.id != message.author.id ||
-              message.type == types.MessageType.image
-          ? const Color(0xfff5f5f7)
-          : const Color(0xff6f61e8),
-      margin: nextMessageInGroup
-          ? const BubbleEdges.symmetric(horizontal: 6)
-          : null,
-      nip: nextMessageInGroup
-          ? BubbleNip.no
-          : _user.id != message.author.id
-              ? BubbleNip.leftBottom
-              : BubbleNip.rightBottom,
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        bottom: false,
-        child: Chat(
-          messages: _messages,
-          onSendPressed: _handleSendPressed,
-          user: _user,
-          bubbleBuilder: _bubbleBuilder,
-          onEndReached: _handleEndReached,
-          showUserAvatars: true,
-          showUserNames: true,
-        ),
-      ),
     );
   }
 }
