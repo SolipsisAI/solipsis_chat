6572cb8ccf17335986de370c97bf2148bbd060c6
Bitjockey <fastjack@jackpoint.network>
2022-02-02 00:07:05 -0500

    Move helper into utils file

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,11 @@
 import 'dart:developer' as logger;
 import 'dart:convert';
-import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:flutter_chat_types/flutter_chat_types.dart' as types;
 import 'package:flutter_chat_ui/flutter_chat_ui.dart';
 import 'package:bubble/bubble.dart';
 import 'package:http/http.dart' as http;
-
-// For the testing purposes, you should probably use https://pub.dev/packages/uuid
-String randomString() {
-  final random = Random.secure();
-  final values = List<int>.generate(16, (i) => random.nextInt(255));
-  return base64UrlEncode(values);
-}
+import 'utils.dart';
 
 void main() {
   runApp(const SolipsisChat());
