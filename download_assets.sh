4e12b8dce6590a22a50297830336b7f2ba45a50b
Bitjockey <bitjockey@jackpoint.network>
2022-02-27 17:33:26 -0500

    Download correct file

--- download_assets.sh
+++ download_assets.sh
@@ -6,7 +6,7 @@ MODEL_FILENAME="text_classification.tflite"
 VOCAB_FILENAME="text_classification_vocab.txt"
 
 MODEL_URL="https://storage.googleapis.com/download.tensorflow.org/models/tflite/text_classification/${MODEL_FILENAME}"
-VOCAB_URL="https://github.com/am15h/tflite_flutter_plugin/blob/master/example/assets/${VOCAB_FILENAME}"
+VOCAB_URL="https://raw.githubusercontent.com/am15h/tflite_flutter_plugin/master/example/assets/${VOCAB_FILENAME}"
 
 download () {
     curl -o "${ASSETS_DIR}/$1" "$2"
