ddcc32552b4429a758afadd39629cc288a47c204
Bitjockey <fastjack@jackpoint.network>
2022-02-27 15:52:42 -0500

    Initialize classifier

---
+++ lib/classifier.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/services.dart';
+
+class Classifier {
+  final _vocabFile = 'text_classification_vocab.txt';
+
+  // Maximum length of sentence
+  final int _sentenceLen = 256;
+
+  final String start = '<START>';
+  final String pad = '<PAD>';
+  final String unk = '<UNKNOWN>';
+
+  late Map<String, int> _dict;
+
+  Classifier() {
+    _loadDictionary();
+  }
+
+  void _loadDictionary() async {
+    final vocab = await rootBundle.loadString('assets/$_vocabFile');
+    var dict = <String, int>{};
+    final vocabList = vocab.split('\n');
+    for (var i = 0; i < vocabList.length; i++) {
+      var entry = vocabList[i].trim().split(' ');
+      dict[entry[0]] = int.parse(entry[1]);
+    }
+    _dict = dict;
+    print('Dictionary loaded successfully');
+  }
+
+  List<List<double>> tokenizeInputText(String text) {
+    // Whitespace tokenization
+    final toks = text.split(' ');
+
+    // Create a list of length==_sentenceLen filled with the value <pad>
+    var vec = List<double>.filled(_sentenceLen, _dict[pad]!.toDouble());
+
+    var index = 0;
+    if (_dict.containsKey(start)) {
+      vec[index++] = _dict[start]!.toDouble();
+    }
+
+    // For each word in sentence find corresponding index in dict
+    for (var tok in toks) {
+      if (index > _sentenceLen) {
+        break;
+      }
+      vec[index++] = _dict.containsKey(tok)
+          ? _dict[tok]!.toDouble()
+          : _dict[unk]!.toDouble();
+    }
+
+    // returning List<List<double>> as our interpreter input tensor expects the shape, [1,256]
+    return [vec];
+  }
+}
