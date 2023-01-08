8c4857759cc0ed8dc40f1ad2bd3957816a1f500b
Bitjockey <fastjack@jackpoint.network>
2023-01-08 11:06:25 -0500

    Set up code for inference

--- lib/classifier.dart
+++ lib/classifier.dart
@@ -31,12 +31,6 @@ class Classifier {
   /// Padding the image to transform into square
   late int padSize;
 
-  /// Shapes of output tensors
-  late List<List<int>> _outputShapes;
-
-  /// Types of output tensors
-  late List<TfLiteType> _outputTypes;
-
   /// Number of results to show
   static const int NUM_RESULTS = 10;
 
@@ -54,14 +48,6 @@ class Classifier {
               modelFile,
               options: InterpreterOptions()..threads = 4,
             );
-
-      var outputTensors = _interpreter.getOutputTensors();
-      _outputShapes = [];
-      _outputTypes = [];
-      outputTensors.forEach((tensor) {
-        _outputShapes.add(tensor.shape);
-        _outputTypes.add(tensor.type);
-      });
     } catch (e) {
       print("Error while creating interpreter: $e");
     }
@@ -152,90 +138,26 @@ class Classifier {
   }
 
   /// Runs object detection on the input image
-  Map<String, dynamic> predict(imageLib.Image image) {
+  String predict(String rawText) {
     var predictStartTime = DateTime.now().millisecondsSinceEpoch;
-
-    if (_interpreter == null) {
-      print("Interpreter not initialized");
-      return null;
-    }
-
     var preProcessStart = DateTime.now().millisecondsSinceEpoch;
 
-    // Create TensorImage from image
-    TensorImage inputImage = TensorImage.fromImage(image);
-
     // Pre-process TensorImage
-    inputImage = getProcessedImage(inputImage);
+    List<List<int>> input = preprocessText(rawText);
+
+    // Setup output
+    var output = List<double>.filled(6, 0).reshape([1, 6]);
 
     var preProcessElapsedTime =
         DateTime.now().millisecondsSinceEpoch - preProcessStart;
 
-    // TensorBuffers for output tensors
-    TensorBuffer outputLocations = TensorBufferFloat(_outputShapes[0]);
-    TensorBuffer outputClasses = TensorBufferFloat(_outputShapes[1]);
-    TensorBuffer outputScores = TensorBufferFloat(_outputShapes[2]);
-    TensorBuffer numLocations = TensorBufferFloat(_outputShapes[3]);
-
-    // Inputs object for runForMultipleInputs
-    // Use [TensorImage.buffer] or [TensorBuffer.buffer] to pass by reference
-    List<Object> inputs = [inputImage.buffer];
-
-    // Outputs map
-    Map<int, Object> outputs = {
-      0: outputLocations.buffer,
-      1: outputClasses.buffer,
-      2: outputScores.buffer,
-      3: numLocations.buffer,
-    };
-
     var inferenceTimeStart = DateTime.now().millisecondsSinceEpoch;
 
     // run inference
-    _interpreter.runForMultipleInputs(inputs, outputs);
+    _interpreter.run(input, output);
 
     var inferenceTimeElapsed =
         DateTime.now().millisecondsSinceEpoch - inferenceTimeStart;
-
-    // Maximum number of results to show
-    int resultsCount = min(NUM_RESULTS, numLocations.getIntValue(0));
-
-    // Using labelOffset = 1 as ??? at index 0
-    int labelOffset = 1;
-
-    // Using bounding box utils for easy conversion of tensorbuffer to List<Rect>
-    List<Rect> locations = BoundingBoxUtils.convert(
-      tensor: outputLocations,
-      valueIndex: [1, 0, 3, 2],
-      boundingBoxAxis: 2,
-      boundingBoxType: BoundingBoxType.BOUNDARIES,
-      coordinateType: CoordinateType.RATIO,
-      height: INPUT_SIZE,
-      width: INPUT_SIZE,
-    );
-
-    List<Recognition> recognitions = [];
-
-    for (int i = 0; i < resultsCount; i++) {
-      // Prediction score
-      var score = outputScores.getDoubleValue(i);
-
-      // Label string
-      var labelIndex = outputClasses.getIntValue(i) + labelOffset;
-      var label = _labels.elementAt(labelIndex);
-
-      if (score > THRESHOLD) {
-        // inverse of rect
-        // [locations] corresponds to the image size 300 X 300
-        // inverseTransformRect transforms it our [inputImage]
-        Rect transformedRect = imageProcessor.inverseTransformRect(
-            locations[i], image.height, image.width);
-
-        recognitions.add(
-          Recognition(i, label, score, transformedRect),
-        );
-      }
-    }
 
     var predictElapsedTime =
         DateTime.now().millisecondsSinceEpoch - predictStartTime;
