04b5983846627e30d5aa7223b1a1ef3386295a70
Bitjockey <bitjockey@jackpoint.network>
2023-01-08 22:44:41 -0500

    camera singleton

--- lib/ui/camera_view_singleton.dart
+++ lib/ui/camera_view_singleton.dart
@@ -2,9 +2,9 @@ import 'dart:ui';
 
 /// Singleton to record size related data
 class CameraViewSingleton {
-  static double ratio;
-  static Size screenSize;
-  static Size inputImageSize;
+  static double ratio = 1;
+  static Size screenSize = Size(500, 500);
+  static Size inputImageSize = Size(500, 500);
   static Size get actualPreviewSize =>
       Size(screenSize.width, screenSize.width * ratio);
 }
