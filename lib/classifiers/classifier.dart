4857a277373e76ce4cf15dcd06a1e865b7ee115f
AJ <bitjockey@jackpoint.network>
2023-01-30 01:01:28 -0500

    Move math utils to classifier

--- lib/classifiers/classifier.dart
+++ lib/classifiers/classifier.dart
@@ -1,5 +1,28 @@
 import 'package:flutter/services.dart';
 import 'package:tflite_flutter/tflite_flutter.dart';
+import 'package:ml_linalg/linalg.dart';
+
+Vector softmax(List<double> output) {
+  // Compute the softmax
+  final vector1 = Vector.fromList(output);
+  final expVec = vector1.exp();
+  final sum = expVec.toList().reduce((a, b) => a + b);
+  final result = expVec.scalarDiv(sum);
+  print(result.max());
+  return result;
+}
+
+int argMax(Vector input) {
+  double maxVal = input.max();
+  var index = 0;
+  for (var i = 0; i < input.length; i++) {
+    if (input[i] == maxVal) {
+      index = i;
+      break;
+    }
+  }
+  return index;
+}
 
 class Classifier {
   late String vocabFile;
